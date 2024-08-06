590922c475b3d1090830b6edb4540258ad249f3e
IzukaAyaka <168893606+IzukaAyaka@users.noreply.github.com>
2024-08-06 09:13:59 +0900

    Add files via upload

---
+++ kadai_005/bmi.js
@@ -0,0 +1,13 @@
+// 体重
+let bodyWeight='68'
+// 身長
+let height='1.7'
+// bmi
+let bmi=bodyWeight/(height*height)
+// コンソールへの出力
+console.log(bmi)
+
+// bmiという変数を宣言するかを迷いました。
+// しない場合はこの形でも同じ結果が得られますよね？
+// コンソールへの出力
+// console.log(bodyWeight/(height*height));
